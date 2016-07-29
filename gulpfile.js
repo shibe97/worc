39b3cf81e08dd5a18f36cf7201c532dcfe7b928a
kazshiba <kazshiba@yahoo-corp.jp>
2016-07-29 10:20:50 +0900

    [update] use eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp          = require('gulp');
-var sass          = require('gulp-sass');
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+var eslint = require('gulp-eslint');
 
 gulp.task('watch', function(){
     gulp.watch('./app/styles/**/*.scss', ['sass']);
@@ -9,6 +10,13 @@ gulp.task('sass', function () {
     gulp.src('./app/styles/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'))
+});
+
+gulp.task('lint', function(){
+    return gulp.src('./app/**/*.js')
+        .pipe(eslint({useEslintrc: true}))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('default',['watch']);
