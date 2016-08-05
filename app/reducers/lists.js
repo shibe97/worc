61680f2b0ca3afc2639236e5fda80e26d790a470
kazshiba <kazshiba@yahoo-corp.jp>
2016-08-05 10:24:04 +0900

    [clean] improve code

--- app/reducers/lists.js
+++ app/reducers/lists.js
@@ -1,39 +1,59 @@
-const initialItems = {
+const initialState = {
   gettingList     : false,
   list            : [],
   gettingStatuses : false,
   statuses        : []
 };
 
-export default function listsReducer (items = initialItems, action) {
-  let _items = Object.assign({}, items);
+export default function listsReducer (state = initialState, action) {
   switch (action.type) {
     case 'SYSTEM_ERROR':
-      _items.gettingList = false;
-      _items.gettingStatuses = false;
-      break;
+      return {
+        ...state,
+        gettingList     : false,
+        gettingStatuses : false
+      };
+
     case 'REQUEST_GET_LIST':
-      _items.gettingList = true;
-      break;
+      return {
+        ...state,
+        gettingList     : true
+      };
+
     case 'SUCCESS_GET_LIST':
-      _items.gettingList = false;
-      _items.list = action.payload.data;
-      break;
+      return {
+        ...state,
+        gettingList : false,
+        list : action.payload.data
+      };
+
     case 'FAILURE_GET_LIST':
-      _items.gettingList = false;
-      break;
+      return {
+        ...state,
+        gettingList : false
+      };
+
     case 'REQUEST_GET_LISTS_STATUSES':
-      _items.gettingStatuses = true;
-      break;
+      return {
+        ...state,
+        gettingStatuses : true
+      };
+
     case 'SUCCESS_GET_LISTS_STATUSES':
-      _items.gettingStatuses = false;
-      _items.statuses = action.payload.data;
-      break;
+      return {
+        ...state,
+        gettingStatuses : false,
+        statuses : action.payload.data
+      };
+
     case 'FAILURE_GET_LISTS_STATUSES':
-      _items.gettingStatuses = false;
-      break;
+      return {
+        ...state,
+        gettingStatuses : false
+      };
+
     default:
       break;
   }
-  return _items;
+  return state;
 };
