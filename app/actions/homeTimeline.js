eaaee17ccdf001a64bc407e54d73d1a9d46ea6cb
Kazuki Shibata <k.shibata97@gmail.com>
2016-09-22 22:04:39 +0900

    [delete] merge timeline actions

--- app/actions/homeTimeline.js
+++
@@ -1,8 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const REQUEST_GET_HOME_TIMELINE = 'REQUEST_GET_HOME_TIMELINE';
-export const SUCCESS_GET_HOME_TIMELINE = 'SUCCESS_GET_HOME_TIMELINE';
-export const FAILURE_GET_HOME_TIMELINE = 'FAILURE_GET_HOME_TIMELINE';
-export const requestGetHomeTimeline = createAction(REQUEST_GET_HOME_TIMELINE);
-export const successGetHomeTimeline = createAction(SUCCESS_GET_HOME_TIMELINE);
-export const failureGetHomeTimeline = createAction(FAILURE_GET_HOME_TIMELINE);
--- app/actions/mentionsTimeline.js
+++
@@ -1,8 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const REQUEST_GET_MENTIONS_TIMELINE = 'REQUEST_GET_MENTIONS_TIMELINE';
-export const SUCCESS_GET_MENTIONS_TIMELINE = 'SUCCESS_GET_MENTIONS_TIMELINE';
-export const FAILURE_GET_MENTIONS_TIMELINE = 'FAILURE_GET_MENTIONS_TIMELINE';
-export const requestGetMentionsTimeline = createAction(REQUEST_GET_MENTIONS_TIMELINE);
-export const successGetMentionsTimeline = createAction(SUCCESS_GET_MENTIONS_TIMELINE);
-export const failureGetMentionsTimeline = createAction(FAILURE_GET_MENTIONS_TIMELINE);
