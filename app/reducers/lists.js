61cf8bfeabc14ecf7be0980c05241a760d66ceaa
kazshiba <kazshiba@yahoo-corp.jp>
2016-10-20 15:45:48 +0900

    [fix] retweeted tweet bug

--- app/reducers/lists.js
+++ app/reducers/lists.js
@@ -56,7 +56,15 @@ export default function listsReducer (state = initialState, action) {
       return {
         ...state,
         statuses : state.statuses.map((tweet) => {
-          if (tweet.id_str === action.payload.id) {
+          if (tweet.retweeted_status && tweet.retweeted_status.id_str === action.payload.id) {
+            return Object.assign({}, tweet, {
+              retweeted_status : {
+                ...tweet.retweeted_status,
+                favorite_count : tweet.retweeted_status.favorite_count + 1,
+                favorited : true
+              }
+            });
+          } else if (tweet.id_str === action.payload.id) {
             return Object.assign({}, tweet, {
               favorite_count : tweet.favorite_count + 1,
               favorited : true
@@ -70,10 +78,40 @@ export default function listsReducer (state = initialState, action) {
       return {
         ...state,
         statuses : state.statuses.map((tweet) => {
-          if (tweet.id_str === action.payload.id) {
+          if (tweet.retweeted_status && tweet.retweeted_status.id_str === action.payload.id) {
+            return Object.assign({}, tweet, {
+              retweeted_status : {
+                ...tweet.retweeted_status,
+                favorite_count : tweet.retweeted_status.favorite_count - 1,
+                favorited : false
+              }
+            });
+          } else if (tweet.id_str === action.payload.id) {
             return Object.assign({}, tweet, {
               favorite_count : tweet.favorite_count - 1,
               favorited : false
+            });
+          }
+          return tweet;
+        })
+      };
+
+    case 'SUCCESS_POST_RETWEET':
+      return {
+        ...state,
+        statuses : state.statuses.map((tweet) => {
+          if (tweet.retweeted_status && tweet.retweeted_status.id_str === action.payload.id) {
+            return Object.assign({}, tweet, {
+              retweeted_status : {
+                ...tweet.retweeted_status,
+                retweet_count : tweet.retweeted_status.retweet_count + 1,
+                retweeted : true
+              }
+            });
+          } else if (tweet.id_str === action.payload.id) {
+            return Object.assign({}, tweet, {
+              retweet_count : tweet.retweet_count + 1,
+              retweeted : true
             });
           }
           return tweet;
