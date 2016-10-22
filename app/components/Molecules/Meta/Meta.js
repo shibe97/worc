5b546e1a9f30a2851ad6bec0ebb204f8bb5cf090
Kazuki Shibata <k.shibata97@gmail.com>
2016-10-22 23:17:18 +0900

    [update] create meta

--- app/components/Molecules/Meta/Meta.js
+++ app/components/Molecules/Meta/Meta.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Name from '../../Atoms/Name/Name';
 import ScreenName from '../../Atoms/ScreenName/ScreenName';
 import CreatedAt from '../../Atoms/CreatedAt/CreatedAt';
+import styles from './meta.css';
 
 export default ({ tweet = {} }) => (
-  <div>
+  <div className={styles.meta}>
     <Name><a href="javascript:void(0);" onClick={() => this.props.setUser(tweet.user)}>{tweet.user.name}</a></Name>
     <span className="ml5px"><ScreenName>@{tweet.user.screen_name}</ScreenName></span>
-    <span className="Tweet__createdAt"><CreatedAt>{tweet.created_at}</CreatedAt></span>
+    <span className={styles.createdAt}><CreatedAt>{tweet.created_at}</CreatedAt></span>
   </div>
 );
