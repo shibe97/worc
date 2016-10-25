79f61b657ec95d46c131284c8ff8f843ee6bfab0
kazshiba <kazshiba@yahoo-corp.jp>
2016-10-25 11:06:37 +0900

    [fix] path

--- app/components/Atoms/RetweetTrigger/RetweetTrigger.js
+++ app/components/Atoms/RetweetTrigger/RetweetTrigger.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './RetweetTrigger.css';
+import styles from './retweetTrigger.css';
 
 export default ({ id_str, retweeted, retweet_count, openRetweetModal }) => (
   <span>
