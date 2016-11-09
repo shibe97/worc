40c783bdfbf3092be2f189e82004643cb56f81d3
Kazuki Shibata <k.shibata97@gmail.com>
2016-11-09 22:20:50 +0900

    [update] toLocaleString

--- app/components/Atoms/FavoriteTrigger/FavoriteTrigger.js
+++ app/components/Atoms/FavoriteTrigger/FavoriteTrigger.js
@@ -15,7 +15,7 @@ export default ({ id_str, favorited, favorite_count, requestPostFavoritesDestroy
         <svg height="12px" version="1.1" viewBox="0 0 23.218 20.776"><path d="M11.608,20.776c-22.647-12.354-6.268-27.713,0-17.369  C17.877-6.937,34.257,8.422,11.608,20.776z"/></svg>
       </a>
       <a href="javascript:void(0);" className={styles.actionCount}>
-        {favorite_count}
+        {favorite_count.toLocaleString()}
       </a>
     </span>
   );
--- app/components/Atoms/RetweetTrigger/RetweetTrigger.js
+++ app/components/Atoms/RetweetTrigger/RetweetTrigger.js
@@ -7,7 +7,7 @@ export default ({ id_str, retweeted, retweet_count, openRetweetModal }) => (
       <svg height="10px" version="1.1" viewBox="0 0 100 60"><path d="M24.9,46V19.9H35L17.5,0L0,19.9h10.1V50c0,5.523,4.476,10,10,10H65L52.195,46H24.9z M89.9,40.1V10c0-5.523-4.477-10-10-10 H35l12.804,14h27.295v26.1H65L82.5,60L100,40.1H89.9z"/></svg>
     </a>
     <a href="javascript:void(0);" className={styles.actionCount}>
-      {retweet_count}
+      {retweet_count.toLocaleString()}
     </a>
   </span>
 );
