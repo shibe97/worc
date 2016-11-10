b37f24eb351c8e6066b53f760debe5d9185e8341
Kazuki Shibata <k.shibata97@gmail.com>
2016-11-10 23:00:54 +0900

    [revert] list stream

--- app/components/ListsTimeline.js
+++ app/components/ListsTimeline.js
@@ -4,7 +4,7 @@ import styles from './listsTimeline.css';
 
 export default class ListsTimeline extends Component {
   componentWillMount() {
-    this.props.requestStreamLists();
+    this.props.requestStreamSiteFollow();
   }
 
   render() {
