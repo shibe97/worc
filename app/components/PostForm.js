6da068f98f0f56a81a69544080a091b840cf4863
Kazuki Shibata <k.shibata97@gmail.com>
2016-11-11 22:44:16 +0900

    [update] exchange button component

--- app/components/PostForm.js
+++ app/components/PostForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from './Atoms/Button/Button';
 
 export default class PostForm extends Component {
   inputUpdate(e) {
@@ -15,13 +16,7 @@ export default class PostForm extends Component {
         <textarea className="PostForm__textarea" placeholder="What's happening?" ref="text" value={this.props.data.update} onChange={this.inputUpdate.bind(this)}></textarea>
         <div className="PostForm__actions">
           <span className={this.props.data.remainingCharacters < 0 ? 'PostForm__overLength mr15px' : 'mr15px'}>{this.props.data.remainingCharacters}</span>
-          {
-            this.props.data.postingUpdate
-            ?
-            <input className="Button Button__normal disabled" type="button" value="Posting..." onClick={this.postUpdate.bind(this)} />
-            :
-            <input className="Button Button__normal" type="button" value="Post" onClick={this.postUpdate.bind(this)} />
-          }
+          <Button value="Post" disabledValue="Posting..." onClick={() => this.postUpdate()} disabled={this.props.data.postingUpdate} />
         </div>
       </form>
     );
