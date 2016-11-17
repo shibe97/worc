312425e51e09e975dba2af0c1f069e8de9ba1a16
Kazuki Shibata <k.shibata97@gmail.com>
2016-11-17 23:43:29 +0900

    [update] textarea component

--- app/components/PostForm.js
+++ app/components/PostForm.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
+import Textarea from './Atoms/Textarea/Textarea';
 import Button from './Atoms/Button/Button';
 
 export default class PostForm extends Component {
-  inputUpdate(e) {
-    this.props.inputUpdate(e.target.value);
-  }
-
-  postUpdate() {
-    this.props.requestPostUpdate(this.refs.text.value);
-  }
-
   render() {
     return (
       <form className="PostForm">
-        <textarea className="PostForm__textarea" placeholder="What's happening?" ref="text" value={this.props.data.update} onChange={this.inputUpdate.bind(this)}></textarea>
+        <Textarea value={this.props.data.update} inputUpdate={this.props.inputUpdate} />
         <div className="PostForm__actions">
           <span className={this.props.data.remainingCharacters < 0 ? 'PostForm__overLength mr15px' : 'mr15px'}>{this.props.data.remainingCharacters}</span>
           <Button
             type="normal"
             value="Post"
-            onClick={() => this.postUpdate()}
+            onClick={() => this.props.requestPostUpdate(this.props.data.update)}
             disabled={this.props.data.postingUpdate || this.props.data.remainingCharacters === 140 || this.props.data.remainingCharacters < 0} />
         </div>
       </form>
