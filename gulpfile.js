7a9fb2e7883991f90141b8a9ceff065f68539553
Kazuki Shibata <k.shibata97@gmail.com>
2016-11-23 21:36:25 +0900

    [remove] gulp-eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp   = require('gulp');
 var sass   = require('gulp-sass');
-var eslint = require('gulp-eslint');
 
 gulp.task('watch', function() {
     gulp.watch('./app/styles/**/*.scss', ['sass']);
@@ -10,13 +9,6 @@ gulp.task('sass', function() {
     gulp.src('./app/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'))
-});
-
-gulp.task('lint', function() {
-    return gulp.src('./app/**/*.js')
-        .pipe(eslint({useEslintrc: true}))
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
 });
 
 gulp.task('default',['watch']);
