2edf3178891bed33ebd970a531aac3d63917a841
Kazuki Shibata <k.shibata97@gmail.com>
2016-12-08 21:36:54 +0900

    [update] delete comment

--- app/main.js
+++ app/main.js
@@ -3,17 +3,14 @@ import storage from 'electron-json-storage';
 import Authentication from './utils/authentication';
 import menu from './menu';
 
-// メインウィンドウはGCされないようにグローバル宣言
 let mainWindow = null;
 
-// 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-// Electronの初期化完了後に実行
 app.on('ready', () => {
   Menu.setApplicationMenu(menu);
   new Authentication((token) => {
@@ -21,7 +18,6 @@ app.on('ready', () => {
       if (error) throw error;
     });
 
-    // メイン画面の表示。ウィンドウの幅、高さを指定できる
     mainWindow = new BrowserWindow({
       x: 0,
       y: 0,
@@ -40,7 +36,6 @@ app.on('ready', () => {
     mainWindow.loadURL(`file://${__dirname}/index.html`);
     // mainWindow.openDevTools(true);
 
-    // ウィンドウが閉じられたらアプリも終了
     mainWindow.on('closed', () => {
       mainWindow = null;
     });
