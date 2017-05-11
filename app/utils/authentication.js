7e79fbc1cf04dfe9bdc19cdec28b42563b3a6b09
Kazuki Shibata <k.shibata97@gmail.com>
2017-05-11 19:06:10 +0900

    [update] store data

--- app/utils/authentication.js
+++ app/utils/authentication.js
@@ -1,5 +1,6 @@
 import { BrowserWindow, screen } from 'electron';
 import NodeTwitterApi from 'node-twitter-api';
+import storage from 'electron-json-storage';
 
 let authWindow = null;
 
@@ -11,46 +12,52 @@ export default class Authentication {
       consumerSecret: 'lhGbkm21g5e65fUKk3nAvG0DTQMXMQOneNymhmkex5DqFxipHy',
     });
 
-    nodeTwitterApi.getRequestToken((error, requestToken, requestTokenSecret) => {
-      let oldWindow;
-      const url = nodeTwitterApi.getAuthUrl(requestToken);
-      if (authWindow) {
-        oldWindow = authWindow;
-      }
+    storage.get('auth', (err, data) => {
+      if (err || Object.keys(data).length === 0) {
+        nodeTwitterApi.getRequestToken((error, requestToken, requestTokenSecret) => {
+          let oldWindow;
+          const url = nodeTwitterApi.getAuthUrl(requestToken);
+          if (authWindow) {
+            oldWindow = authWindow;
+          }
 
-      authWindow = new BrowserWindow({
-        width: screen.getPrimaryDisplay().size.width,
-        height: screen.getPrimaryDisplay().size.height
-      });
+          authWindow = new BrowserWindow({
+            width: screen.getPrimaryDisplay().size.width,
+            height: screen.getPrimaryDisplay().size.height
+          });
 
-      authWindow.webContents.on('will-navigate', (event, _url) => {
-        const matched = _url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/);
-        if (matched) {
-          event.preventDefault();
+          authWindow.webContents.on('will-navigate', (event, _url) => {
+            const matched = _url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/);
+            if (matched) {
+              event.preventDefault();
 
-          nodeTwitterApi.getAccessToken(requestToken, requestTokenSecret, matched[2], (_error, accessToken, accessTokenSecret, results) => {
-            if (error) {
-              new Authentication(callback);
-            } else {
-              const { user_id, screen_name } = results;
-              const token = { accessToken, accessTokenSecret, user_id, screen_name };
-              callback(token);
-              if (authWindow) {
-                authWindow.close();
-              }
+              nodeTwitterApi.getAccessToken(requestToken, requestTokenSecret, matched[2], (_error, accessToken, accessTokenSecret, results) => {
+                if (error) {
+                  new Authentication(callback);
+                } else {
+                  const { user_id, screen_name } = results;
+                  const token = { accessToken, accessTokenSecret, user_id, screen_name };
+                  callback(token);
+                  if (authWindow) {
+                    authWindow.close();
+                  }
+                }
+              });
             }
           });
-        }
-      });
 
-      authWindow.on('closed', () => {
-        authWindow = null;
-      });
+          authWindow.on('closed', () => {
+            authWindow = null;
+          });
 
-      if (oldWindow) {
-        oldWindow.close();
+          if (oldWindow) {
+            oldWindow.close();
+          }
+          authWindow.loadURL(`${url}&force_login=true`);
+        });
+      } else {
+        callback(data);
       }
-      authWindow.loadURL(`${url}&force_login=true`);
     });
   }
 }
